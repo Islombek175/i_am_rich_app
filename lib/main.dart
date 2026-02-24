bebebab6ce027b714c97856c51e5c4244476db5e
islombek <matkaimovislambek@gmail.com>
2026-02-24 18:10:11 +0500

    done

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I AM RICH', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(child: Image(image: AssetImage('images/diamond.png'))),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
